c1cd98023ab8416b23bb413c76b848ec40e31981
Ryan Hantak <rhantak@gmail.com>
2019-11-02 22:26:29 -0600

    Add method to course to sort enrolled students by their grade

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course < ApplicationRecord
   validates_presence_of :name
   has_many :student_courses
   has_many :students, through: :student_courses
+
+  def students_by_grade
+    students.order('student_courses.grade DESC')
+  end
 end
